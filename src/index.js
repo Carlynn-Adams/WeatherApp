5d3ad28372d521148874c8e6960e9780215c28f1
Carlynn-Adams <carlynnadams25@gmail.com>
2024-04-17 15:34:05 +0200

    Added the search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
+function refreshWeather(response) {
+let temperatureElement = document.querySelector("#temperature");
+let temperature = response.data.temperature.current;
+let cityElement = document.querySelector("#city");
+
+cityElement.innerHTML = response.data.city;
+temperatureElement.innerHTML = Math.round(temperature);
+
+
+}
+
+
+
+function searchCity(city) {
+let apiKey = "fab70e22d595583415ddd8b1ba4t4a9o";
+let apiURL =
+  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(refreshWeather);
+}
+
+
 function handleSearchSubmit(event) {
   event.preventDefault();
-
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Cape Town");
